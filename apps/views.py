6b0fac76565f05af45e5f15a9135efd244dd676d
asifsetu <setu.asif@gmail.com>
2017-07-18 12:42:43 +0300

    First Commit

---
+++ apps/views.py
@@ -0,0 +1,47 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+import json, requests
+from time import gmtime, strftime
+# Create your views here.
+
+def index(request):
+    url = 'https://rata.digitraffic.fi/api/v1/live-trains?station=SLO'
+    resp = requests.get(url=url)
+    data = json.loads(resp.text)
+    date = strftime("%Y-%m-%d", gmtime())
+    finaldata = {}
+    case_list = []
+    for i in data:
+        trainno = i['trainNumber']
+        url1 = 'https://rata.digitraffic.fi/api/v1/compositions/'+str(trainno)+'?departure_date='+date
+        resp1 = requests.get(url=url1)
+        data1 = json.loads(resp1.text)
+        try:
+            wagon = []
+            case = {'trainNumber' : data1['trainNumber'],
+                    'trainType': data1['trainType'],
+                    'stationShortCode': data1['journeySections'][0]['beginTimeTableRow']['stationShortCode'],
+                    'stationShortCode2' : data1['journeySections'][0]['endTimeTableRow']['stationShortCode'],
+                    'locomotiveType': data1['journeySections'][0]['locomotives'][0]['locomotiveType'],
+                    'powerType': data1['journeySections'][0]['locomotives'][0]['powerType'],
+                    'wagons' : wagon,
+                    'maximumSpeed': data1['journeySections'][0]['maximumSpeed'],
+                    'totalLength': data1['journeySections'][0]['totalLength']
+                    }
+            case_list.append(case)
+
+            try:
+                for w in data1['journeySections'][0]['wagons']:
+                    print(w['wagonType'])
+                    wgn = {'wagonType': w['wagonType']}
+                    wagon.append(wgn)
+            except:
+                pass
+        except:
+            pass
+    print(case_list)
+    context = {
+        'data': data,
+        'finaldata': case_list
+    }
+    return render(request, 'home.html', context)
